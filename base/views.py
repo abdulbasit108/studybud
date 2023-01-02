669a169e3c013d9265c249fe751b427bd34f4dac
Abdul Basit <abdbasit108@gmail.com>
2023-01-02 23:13:11 +0500

    Day 2: 2 Jan 2023

--- base/views.py
+++ base/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 # Create your views here.
 
+# rooms = [
+#    {'id':1, 'name':'Lets Learn Python!'},
+#    {'id':2, 'name':'Django Crash Course'},
+#    {'id':3, 'name':'Supervised Learning'},
+# ]
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context )
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
